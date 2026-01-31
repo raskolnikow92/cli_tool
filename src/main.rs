856148347403b41d7400307d5b34341573cc7411
kaisc <kai.schuleit@hotmail.de>
2026-02-01 00:12:07 +0100

    implement cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+use walkdir;
+
+#[derive(Parser)]
+#[command(name = "rust_cli_tool")]
+#[command(about = "High-Performance File Analyzer in Rust", long_about = None)]
+struct Cli {
+    /// Directory to scan
+    #[arg(short, long)]
+    path: String,
+
+    /// Pattern to search
+    #[arg(short = 't', long)]
+    pattern: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("Scanning directory: {}", cli.path);
+    println!("Looking for pattern: {}", cli.pattern);
+   
 }
